8d112b31b39cb6e5b02355a8706563f2cf1754ad
malchak <michaelmalchak@gmail.com>
2013-11-04 10:31:00 -0800

    show only active todo instances

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,6 +1,6 @@
 Todos.Router.map(function(){
 	this.resource('todos', { path: '/' }, function(){
-		
+		this.route('active')
 	});
 });
 
@@ -9,3 +9,15 @@ Todos.TodosRoute = Ember.Route.extend({
 		this.store.find('todo');
 	}
 })
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.filter('todo', function(todo){
+			return !todo.get('isCompleted');
+		});
+	},
+
+	renderTemplate: function(controller){
+		this.render('todos/index', {controller: controller});
+	}
+});
